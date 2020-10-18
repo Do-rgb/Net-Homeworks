2d8cb4ce56826806ea1faf8cc6d7241686af036d
dimigphone@gmail.com <dimigphone@gmail.com>
2020-10-18 21:03:21 +0400

    Добавил проект для выполнения 2-го задания

---
+++ HW15_Serialization/CUITask2/Entities/Contacts.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml.Serialization;
+
+namespace CUITask2.Entities
+{
+    [XmlRoot(ElementName="CONTACTS")]
+    public class Contacts {
+        [XmlElement(ElementName="SmsPhoneNumber")]
+        public int SmsPhoneNumber { get; set; }
+        [XmlElement(ElementName="PhoneNeedActualize")]
+        public bool PhoneNeedActualize { get; set; }
+    }
+}
