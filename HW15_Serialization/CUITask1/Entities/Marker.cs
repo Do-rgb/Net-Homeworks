22661dcca89eacaccd30269da054194bb73e4e04
dimigphone@gmail.com <dimigphone@gmail.com>
2020-10-18 20:54:21 +0400

    Реализация первого задания.

---
+++ HW15_Serialization/CUITask1/Entities/Marker.cs
@@ -0,0 +1,30 @@
+﻿using CUITask1.Enums;
+
+namespace CUITask1.Entities
+{
+    /// <summary>
+    ///     Маркер игрока
+    /// </summary>
+    public class Marker
+    {
+        /// <summary>
+        ///     Название маркера
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        ///     Координаты маркера
+        /// </summary>
+        public MarkerCoordinate Coordinates { get; set; }
+
+        /// <summary>
+        ///     Тип маркера
+        /// </summary>
+        public MarkerType Type { get; set; }
+
+        /// <summary>
+        ///     Размер маркера
+        /// </summary>
+        public MarkerSize Size { get; set; }
+    }
+}
---
+++ HW15_Serialization/CUITask1/Entities/MarkerCoordinate.cs
@@ -0,0 +1,23 @@
+﻿namespace CUITask1.Entities
+{
+    /// <summary>
+    ///     Координаты маркера
+    /// </summary>
+    public class MarkerCoordinate
+    {
+        /// <summary>
+        ///     X координата
+        /// </summary>
+        public double X { get; set; }
+
+        /// <summary>
+        ///     Y координата
+        /// </summary>
+        public double Y { get; set; }
+
+        /// <summary>
+        ///     Z координата
+        /// </summary>
+        public double Z { get; set; }
+    }
+}
